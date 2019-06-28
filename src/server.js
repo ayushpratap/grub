11f6f173b23c29018b3c0293a8154958af32d756
Ayush Pratap <ayushs56@gmail.com>
2019-06-29 01:01:51 +0530

    Started the rewrite

---
+++ src/server.js
@@ -0,0 +1,28 @@
+/**
+ * This file handles the starting and working of HTTPS server.
+ */
+
+ // Require components
+ require('magic-globals');
+ const EXPRESS  = require('express');
+ const FS       = require('fs');
+ const HTTPS    = require('https');
+ const IP       = require('ip');
+ const APP      = require('./app/app');
+ const CONFIG   = require('./config/config');
+ const logger   = require('./config/logger');
+ const server   = EXPRESS();
+
+
+ server.use(EXPRESS.json());
+ server.use(EXPRESS.urlencoded({extended:true}));
+
+ const privateKey = FS.readFileSync('./certs/server.key');
+ const certificate = FS.readFileSync('./certs/server.crt');
+ const credentials = {key:privateKey,cert:certificate};
+ const httpsServer = HTTPS.createServer(credentials,server);
+ 
+ server.all('*',APP);
+ httpsServer.listen(CONFIG.port,()=>{
+     logger.info("[%s] , HTTPS server running at [%s:%s]",__file,IP.address(),CONFIG.port);
+ });
