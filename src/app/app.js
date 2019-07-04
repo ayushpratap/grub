f6c5d9b7345d3e227a7adee4106ffcd5bcb20f09
ayushpratap <ayushs56@gmail.com>
2019-07-04 08:12:45 +0530

    intermediate commit

--- src/app/app.js
+++ src/app/app.js
@@ -13,4 +13,23 @@ ROUTER.get('/',(req,res)=>{
     });
 });
 
+ROUTER.post('/register',(req,res)=>{
+    /**
+     * Add a new user
+     */
+    LOGGER.info("[%s] , Request received to add new user",__file);
+    LOGGER.info("[%s] , %o",__file,req.body);
+
+    res.sendStatus(200);
+});
+
+ROUTER.post('/login',(req,res)=>{
+    /**
+     * Existing user trying to login
+     */
+    LOGGER.info("[%s] , Request received to login",__file);
+    LOGGER.info("[%s] , %o",__file,req.body);
+    res.sendStatus(200);
+});
+
 module.exports = ROUTER;
