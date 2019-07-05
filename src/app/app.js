ce2da00de947d028393f3b3db11fc5020a3cd22a
ayushpratap <ayushs56@gmail.com>
2019-07-06 01:34:39 +0530

    Created new views with inheritance

--- src/app/app.js
+++ src/app/app.js
@@ -1,21 +1,23 @@
 require('magic-globals');
 const EXPRESS = require('express');
+const CONFIG = require('../config/config');
 const LOGGER = require('../config/logger');
 const ROUTER = EXPRESS.Router();
 
 ROUTER.get('/',(req,res)=>{
     LOGGER.info("[%s] , Request received at ROUTER.get('/')",__file);
 
-    /*res
-        .status(200)
-        .send("Hello")
-        .end();*/
     res.render("homepage",{
         name:'Ayush',
-        siteName: 'Grub'
+        test_name:CONFIG.test_name,
+        siteName: 'Grub',
+        title:'homepage'
     });
 });
+ROUTER.get('/register',(req,res)=>{
+    LOGGER.info("[%s] , Request received to render register page",__file);
 
+});
 ROUTER.post('/register',(req,res)=>{
     /**
      * Add a new user
@@ -26,13 +28,23 @@ ROUTER.post('/register',(req,res)=>{
     res.sendStatus(200);
 });
 
+ROUTER.get('/login',(req,res)=>{
+    LOGGER.info("[%s] , Request received to render login page",__file);
+    res
+        .status(200)
+        .render("login",{
+            title:"Login Page"
+        })
+});
 ROUTER.post('/login',(req,res)=>{
     /**
      * Existing user trying to login
      */
     LOGGER.info("[%s] , Request received to login",__file);
     LOGGER.info("[%s] , %o",__file,req.body);
-    res.sendStatus(200);
+    res
+    .status(200)
+    .send(req.body);
 });
 
 module.exports = ROUTER;
--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 var CONFIG={};
 
-CONFIG.port     = process.env.HTTPS_PORT    || 8081;
-CONFIG.app_env  = process.env.APP_ENV       || 'dev';
+CONFIG.port         = process.env.HTTPS_PORT    || 8081;
+CONFIG.app_env      = process.env.APP_ENV       || 'dev';
+CONFIG.test_name    = process.env.TEST_NAME     || 'Ayush';
 
 module.exports = CONFIG;
