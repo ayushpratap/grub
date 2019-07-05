8d7e3e899fb96a761794abcb341772fa3f8bec24
ayushpratap <ayushs56@gmail.com>
2019-07-05 17:40:59 +0530

    deployed on aws beanstalk

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 var CONFIG={};
 
-CONFIG.port     = process.env.HTTPS_PORT    || 9000;
+CONFIG.port     = process.env.HTTPS_PORT    || 8081;
 CONFIG.app_env  = process.env.APP_ENV       || 'dev';
 
 module.exports = CONFIG;
