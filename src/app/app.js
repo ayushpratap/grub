1051b06f081da362d08d00e78fc09e541f5829f4
ayushpratap <ayushs56@gmail.com>
2019-07-08 00:41:49 +0530

    intermediate commit

--- src/app/app.js
+++ src/app/app.js
@@ -6,16 +6,38 @@ const ROUTER = EXPRESS.Router();
 
 ROUTER.get('/',(req,res)=>{
     LOGGER.info("[%s] , Request received at ROUTER.get('/')",__file);
-
-    res.render("homepage",{
-        name:'Ayush',
-        test_name:CONFIG.test_name,
-        siteName: 'Grub',
-        title:'homepage'
-    });
+    let session = req.session;
+    if(session.username){
+        return res.redirect('/main');
+    }else{
+       res.render("homepage",{
+            name:'Ayush',
+            test_name:CONFIG.test_name,
+            siteName: 'Grub',
+            title:'homepage'
+        });
+    }
+});
+ROUTER.get('/main',(req,res)=>{
+    if(req.session.username){
+        res
+            .status(200)
+            .render('main',{
+            username:req.session.username
+        });
+    }else{
+        res
+            .status(200)
+            .redirect('/login');
+    }
 });
 ROUTER.get('/register',(req,res)=>{
     LOGGER.info("[%s] , Request received to render register page",__file);
+    res
+        .status(200)
+        .render("register",{
+            title:"Register Page"
+        });
 
 });
 ROUTER.post('/register',(req,res)=>{
@@ -25,7 +47,9 @@ ROUTER.post('/register',(req,res)=>{
     LOGGER.info("[%s] , Request received to add new user",__file);
     LOGGER.info("[%s] , %o",__file,req.body);
 
-    res.sendStatus(200);
+    res
+        .status(200)
+        .send(req.body);
 });
 
 ROUTER.get('/login',(req,res)=>{
@@ -34,7 +58,7 @@ ROUTER.get('/login',(req,res)=>{
         .status(200)
         .render("login",{
             title:"Login Page"
-        })
+        });
 });
 ROUTER.post('/login',(req,res)=>{
     /**
@@ -42,9 +66,24 @@ ROUTER.post('/login',(req,res)=>{
      */
     LOGGER.info("[%s] , Request received to login",__file);
     LOGGER.info("[%s] , %o",__file,req.body);
-    res
-    .status(200)
-    .send(req.body);
+    if(req.session.username){
+        res
+            .status(200)
+            .redirect('/main');
+    }
+    else{
+        if("ayush" == req.body.username){
+            req.session.username = req.body.username;
+            res
+                .status(200)
+                .redirect('/main');
+        }
+        else{
+            res
+                .status(200)
+                .redirect('/')
+        }
+    }
 });
 
 module.exports = ROUTER;
--- src/server.js
+++ src/server.js
@@ -2,31 +2,61 @@
  * This file handles the starting and working of HTTPS server.
  */
 
- // Require components
- require('magic-globals');
- const EXPRESS  = require('express');
- const FS       = require('fs');
- const HTTPS    = require('https');
- const IP       = require('ip');
- const PATH     = require('path');
- const APP      = require('./app/app');
- const CONFIG   = require('./config/config');
- const logger   = require('./config/logger');
- const server   = EXPRESS();
+// Require components
+require('magic-globals');
+const EXPRESS      = require('express');
+const FS           = require('fs');
+const IP           = require('ip');
+const PATH         = require('path');
+const COOKIEPARSER = require('cookie-parser');
+const SESSION      = require('express-session');
+const MONGOOSE     = require('mongoose');
+const MONGOSTORE   = require('connect-mongo')(SESSION);
+const APP          = require('./app/app');
+const CONFIG       = require('./config/config');
+const logger       = require('./config/logger');
+const server       = EXPRESS();
 
- server.set('view engine','pug');
- server.set('views',PATH.join(__dirname,'views'));
- server.use(EXPRESS.static(PATH.join(__dirname,'public')));
- server.use(EXPRESS.json());
- server.use(EXPRESS.urlencoded({extended:true}));
+/**
+ * Connect to database
+ */
+if("NULL" == CONFIG.db_url)
+{
+    logger.error("[%s] , DB_URL is not configured",__file);
+    logger.info("[%s] , Exit the application",__file);
+    process.exit(1);
+}
+MONGOOSE.connect(CONFIG.db_url,{
+    useMongoClient:true
+});
+MONGOOSE.Promise = global.Promise;
+CONFIG.db_connection = MONGOOSE.connection;
 
- //const privateKey = FS.readFileSync('./certs/server.key');
- //const certificate = FS.readFileSync('./certs/server.crt');
- //const credentials = {key:privateKey,cert:certificate};
- //const httpsServer = HTTPS.createServer(credentials,server);
- 
- server.all('*',APP);
 
- server.listen(CONFIG.port,()=>{
-     logger.info("[%s] , HTTPS server running at [%s:%s]",__file,IP.address(),CONFIG.port);
- });
+server.set('view engine','pug');
+server.set('views',PATH.join(__dirname,'views'));
+server.use(EXPRESS.static(PATH.join(__dirname,'public')));
+server.use(EXPRESS.json());
+server.use(EXPRESS.urlencoded({extended:true}));
+server.use(COOKIEPARSER());
+
+if("NULL" == CONFIG.session_secret){
+    logger.error("[%s] , Session secret is not set",__file);
+    logger.info("[%s] , Exit the application",__file);
+    process.exit(1);
+}
+
+server.use(SESSION({
+    secret:CONFIG.session_secret,
+    resave:false,
+    saveUninitialized:true,
+    store: new MONGOSTORE({
+        mongooseConnection:CONFIG.db_connection
+    })
+}));
+
+server.all('*',APP);
+
+server.listen(CONFIG.port,()=>{
+    logger.info("[%s] , HTTPS server running at [%s:%s]",__file,IP.address(),CONFIG.port);
+});
