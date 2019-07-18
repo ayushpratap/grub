5ea5df0b9e46432089d014fa193b03e4afbcf2b2
Ayush Pratap <ayushs56@gmail.com>
2019-07-18 15:04:59 +0530

    Sending test event from client

--- src/helpers/socketEvents.js
+++ src/helpers/socketEvents.js
@@ -8,6 +8,9 @@ const FILE = __file;
 module.exports.onConnection = function(io) {
   const FUNC = 'onConnection';
   io.on('connection', function(socket) {
+    socket.on('test', function(data) {
+      console.log(data);
+    });
     logger.info(`[${FILE}] , [${FUNC}] , socket client connected`);
     logger.info(`[${FILE}] , [${FUNC}] , client id = ${socket.id}`);
   });
